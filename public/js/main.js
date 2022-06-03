a8f4ab9ab0c7c7ca1e56f6f277b3277a3fac59f2
Iana Shtina <sianalucky@gmail.com>
2022-06-03 15:12:36 +0300

    colors

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval("const sum = __webpack_require__(/*! ./modules/sum.js */ \"./src/js/modules/sum.js\");\nconst test = __webpack_require__(/*! ./modules/test.js */ \"./src/js/modules/test.js\");\n\ntest();\nconsole.log(sum(5, 5))\nconsole.log(sum(15,5))\n\n\n//# sourceURL=webpack://iana/./src/js/main.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/sum.js":
+/*!*******************************!*\
+  !*** ./src/js/modules/sum.js ***!
+  \*******************************/
+/***/ (function(module) {
+
+eval("module.exports = (a, b) => a + b;\n\n\n//# sourceURL=webpack://iana/./src/js/modules/sum.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/test.js":
+/*!********************************!*\
+  !*** ./src/js/modules/test.js ***!
+  \********************************/
+/***/ (function(module) {
+
+eval("\nmodule.exports = function() {\n  console.log('HELLO!!')\n};\n\n//# sourceURL=webpack://iana/./src/js/modules/test.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
+/******/ 	
+/******/ })()
+;
